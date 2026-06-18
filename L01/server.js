a6409f8179e6f2510ead0fa56bcbb43dda50e234
navodyamethmali <methmalidissanayake@gmail.com>
2026-06-18 09:57:47 +0530

    Fix L01 folder structure

---
+++ L01/server.js
@@ -0,0 +1,19 @@
+const express = require("express");
+
+const app = express();
+
+app.get("/", (req, res) => {
+    res.send("Hello from Docker Container!");
+});
+
+app.get("/health", (req, res) => {
+    res.json({
+        status: "UP"
+    });
+});
+
+const PORT = 3000;
+
+app.listen(PORT, () => {
+    console.log(`Server running on port ${PORT}`);
+});
